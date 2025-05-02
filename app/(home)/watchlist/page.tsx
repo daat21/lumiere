cb846f2916f64bc1fa1e58875656165abb2f9739
emmanuelphiliposeantony <142146167+emmanuelphiliposeantony@users.noreply.github.com>
2025-05-02 20:15:30 +0930

    Settings and Watchlist pages

--- app/(home)/watchlist/page.tsx
+++ app/(home)/watchlist/page.tsx
@@ -1,3 +1,33 @@
-export default function Watchlist() {
-  return <div>Watchlist</div>
+import { SearchBar } from '@/components/watchlist/searchBar'
+import { MoreToExplore } from '@/components/home/moreToExplore'
+import { getGenresList, getPopularMovies } from '@/lib/tmdb'
+
+export default async function Watchlist() {
+  const genresListPromise = getGenresList()
+  const popularMoviesPromise = getPopularMovies()
+
+  const [genresList, popularMovies] = await Promise.all([
+    genresListPromise,
+    popularMoviesPromise,
+  ])
+  const initialMoreToExploreMovies = popularMovies.slice(10, 19)
+
+  return( 
+    <div className="flex flex-col gap-12">
+      <div>
+      <h1>Watchlist</h1>
+      </div>
+      <div className="flex flex-col">
+        <div>
+          <SearchBar />
+        </div>
+        <div>
+        <MoreToExplore
+          initialGenres={genresList}
+          initialMovies={initialMoreToExploreMovies}
+        />
+        </div>
+      </div>
+    </div>
+  )
 }
