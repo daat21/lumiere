36e593e77459cdb21eb7171e36aeb0dba5f76480
daat21 <daat21@icloud.com>
2025-05-07 21:57:41 +0930

    feat: enhance authentication layout and design
    - Added new dependency 'motion' for animations.

--- app/(auth)/signup/page.tsx
+++ app/(auth)/signup/page.tsx
@@ -1,22 +1,5 @@
-import Link from 'next/link'
-import { GalleryVerticalEnd } from 'lucide-react'
 import { SignupForm } from '@/components/auth/signup-form'
 
 export default function SignupPage() {
-  return (
-    <div className="flex min-h-svh flex-col items-center justify-center gap-6 p-6 md:p-10">
-      <div className="flex w-full max-w-sm flex-col gap-6">
-        <Link
-          href="/"
-          className="flex items-center gap-2 self-center font-medium"
-        >
-          <div className="bg-primary text-primary-foreground flex size-8 items-center justify-center rounded-md">
-            <GalleryVerticalEnd className="size-6" />
-          </div>
-          <span className="text-2xl font-bold">Lumiere</span>
-        </Link>
-        <SignupForm />
-      </div>
-    </div>
-  )
+  return <SignupForm />
 }
