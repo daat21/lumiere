b45a6df8c5a7960945374452cbf411e94f004d10
daat21 <daat21@icloud.com>
2025-05-24 23:30:38 +0930

    fix: watchlist

--- client/app/(home)/page.tsx
+++ client/app/(home)/page.tsx
@@ -3,38 +3,42 @@ import { MostPopular } from '@/components/home/MostPopular'
 import { MoreToExplore } from '@/components/home/MoreToExplore'
 import { Bookmark } from 'lucide-react'
 import { ChatBox } from '@/components/home/ChatBox/ChatBox'
-import { getGenresList, getMovieDetailsByIds, getPopularMovies } from '@/lib/tmdb'
+import {
+  getGenresList,
+  getMovieDetailsByIds,
+  getPopularMovies,
+} from '@/lib/tmdb'
 import { getCurrentUserInfo } from '@/lib/server/user/getCurrentUserInfo'
 import { WatchlistCarousel } from '@/components/home/WatchlistCarousel'
 
 interface MovieDetails {
-  id: number,
-  title: string,
-  poster_path: string | null,
-  vote_average: number,
+  id: number
+  title: string
+  poster_path: string | null
+  vote_average: number
 }
-
 
 export default async function Home() {
   const genresListPromise = getGenresList()
   const popularMoviesPromise = getPopularMovies()
-  const userWatchlist = await getCurrentUserInfo().then((data) => data?.watchlists?.[0]?.movies ?? [])
-                                                    .then((watchlistMovies) => watchlistMovies.movies.map((movie: { movie_id: string }) => {
-                                                      return movie.movie_id
-                                                    }))
-                                                    .then((movieIds) => Promise.all(movieIds.map(getMovieDetailsByIds)))
-                                                    .then((movieDetails: MovieDetails[]) => {
-                                                            const required_details = movieDetails.map((detail
-                                                              : MovieDetails
-                                                            ) => ({
-                                                              id: detail.id,
-                                                              title: detail.title,
-                                                              poster_path: detail.poster_path,
-                                                              vote_average: detail.vote_average
-                                                            }))
-                                                            return required_details;
-                                                          })
-                                                    .catch(()=>[])
+  const userWatchlist = await getCurrentUserInfo()
+    .then(data => data.watchlists[0])
+    .then(watchlistMovies =>
+      watchlistMovies.movies.map((movie: { movie_id: string }) => {
+        return movie.movie_id
+      })
+    )
+    .then(movieIds => Promise.all(movieIds.map(getMovieDetailsByIds)))
+    .then((movieDetails: MovieDetails[]) => {
+      const required_details = movieDetails.map((detail: MovieDetails) => ({
+        id: detail.id,
+        title: detail.title,
+        poster_path: detail.poster_path,
+        vote_average: detail.vote_average,
+      }))
+      return required_details
+    })
+    .catch(() => [])
   const [genresList, popularMovies] = await Promise.all([
     genresListPromise,
     popularMoviesPromise,
@@ -64,16 +68,20 @@ export default async function Home() {
       </div>
       <div>
         <h1>From your watchlist</h1>
-        {userWatchlist.length>0?(<Suspense fallback={<div>Loading...</div>}>
-          <WatchlistCarousel movies={userWatchlist} />
-        </Suspense>):(<div className="my-10 flex flex-col items-center gap-4">
-          <p>
-            <Bookmark className="h-13 w-13" />
-          </p>
-          <p className="text-lg font-semibold">
-            Save shows and movies to keep track of what you want to watch.
-          </p>
-        </div>)}
+        {userWatchlist.length > 0 ? (
+          <Suspense fallback={<div>Loading...</div>}>
+            <WatchlistCarousel movies={userWatchlist} />
+          </Suspense>
+        ) : (
+          <div className="my-10 flex flex-col items-center gap-4">
+            <p>
+              <Bookmark className="h-13 w-13" />
+            </p>
+            <p className="text-lg font-semibold">
+              Save shows and movies to keep track of what you want to watch.
+            </p>
+          </div>
+        )}
       </div>
       <div>
         <h1>More to explore</h1>
--- client/app/(home)/watchlist/page.tsx
+++ client/app/(home)/watchlist/page.tsx
@@ -6,51 +6,50 @@ import { getCurrentUserInfo } from '@/lib/server/user/getCurrentUserInfo'
 import WatchlistClientSide from '@/components/watchlist/watchlistClientSide'
 
 interface RequiredDetailsProps {
-  id: string,
-  title: string,
+  id: string
+  title: string
   backdrop_path: string
 }
 
 interface MovieDetails {
-  id: number,
-  title: string,
-  backdrop_path: string,
+  id: number
+  title: string
+  backdrop_path: string
   genres: {
-    id:string,
+    id: string
     name: string
   }[]
 }
 
 export default async function Watchlist() {
   const genresListPromise = getGenresList()
-  const userWatchlist = await getCurrentUserInfo().then((data) => data?.watchlists?.[0]?.movies ?? [])
-                                                  .then((watchlistMovies) => watchlistMovies.movies.map((movie: { movie_id: string }) => {
-                                                    return movie.movie_id
-                                                  }))
-                                                  .then((movieIds) => Promise.all(movieIds.map(getMovieDetailsByIds)))
-                                                  .then((movieDetails: MovieDetails[]) => {
-                                                          const required_details = movieDetails.map((detail
-                                                            : MovieDetails
-                                                          ) => ({
-                                                            id: detail.id,
-                                                            title: detail.title,
-                                                            backdrop_path: detail.backdrop_path,
-                                                            genres: detail.genres
-                                                          }))
-                                                          return required_details;
-                                                        })
-                                                  .catch(()=>[])
-                              
-  const [genresList] = await Promise.all([
-    genresListPromise
-  ])
-  
-  if (userWatchlist.length == 0) return(<h1>No movies in watchlist, {";_;"}!</h1>)
-  
+  const userWatchlist = await getCurrentUserInfo()
+    .then(data => data.watchlists[0])
+    .then(watchlistMovies =>
+      watchlistMovies.movies.map((movie: { movie_id: string }) => {
+        return movie.movie_id
+      })
+    )
+    .then(movieIds => Promise.all(movieIds.map(getMovieDetailsByIds)))
+    .then((movieDetails: MovieDetails[]) => {
+      const required_details = movieDetails.map((detail: MovieDetails) => ({
+        id: detail.id,
+        title: detail.title,
+        backdrop_path: detail.backdrop_path,
+        genres: detail.genres,
+      }))
+      return required_details
+    })
+    .catch(() => [])
+
+  const [genresList] = await Promise.all([genresListPromise])
+
+  if (userWatchlist.length == 0)
+    return <h1>No movies in watchlist, {';_;'}!</h1>
+
   console.log(userWatchlist)
   console.log(genresList)
   return (
-
     /*<div className="flex flex-col gap-12">
       <div>
         <h1>Watchlist</h1>
@@ -68,6 +67,9 @@ export default async function Watchlist() {
       </div>
     </div>*/
 
-    <WatchlistClientSide genresList={genresList} userWatchlist={userWatchlist}/>
+    <WatchlistClientSide
+      genresList={genresList}
+      userWatchlist={userWatchlist}
+    />
   )
 }
